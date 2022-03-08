7a26d9d6e5ee0a6f5dec0cc21e00429f4fe0e2c7
Strothe <williamstrothe@rocketmortgage.com>
2022-03-08 13:55:10 -0700

    idk

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,11 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".list-group").on("click", "p", function() {
+  var text = $(this).text();
+  console.log(text);
+});
+
 
 
 
